3291f30e16350a7d365afe307e92a3d39bcaca00
Manuela Lorenzi-Kayser <mlorenzikayser@une.edu>
2015-05-28 11:48:32 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Alex</title>
+  <link rel="stylesheet" href="style.css" />
+  <link href='http://fonts.googleapis.com/css?family=Julius+Sans+One' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Crimson+Text:400,700,400italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+   <div class="main">
+      <header>
+         <nav class="navigation" role="navigation">
+            <ul>
+               <li><a href="" class="home-link">Home</a></li>
+               <li><a href=""><img src="images/testaGreca_small.jpg" width="30" /><br />Decorazioni e Dipinti</a></li>
+               <li><a href=""><img src="images/kustomite_small.jpg" width="30" /><br />Kustomite</a></li>
+               <li><a href="">Biografia</a></li>
+               <li><a href="">Contatta</a></li>
+            </ul>
+         </nav>
+      </header>  
+      <section id="slider" class="slider">
+         <ul>
+            <li>
+               <img src="images/slides/mauritius.jpg" alt="Mauritius, Elba - Trompe L'Oeil dipinto a mano" />
+               <a href="" class="button">Trompe L'oeil</a>
+            </li>
+            <li>
+               <img src="images/slides/sansone.jpg" alt="Spiaggia di Sansone, Elba - Murales dipinto a mano" />
+               <a href="" class="button">Murales</a>
+            </li>
+            <li>
+               <img src="images/slides/bacco.jpg" alt="Bacco, Elba - Affresco" />
+               <a href="" class="button">Affreschi</a>
+            </li>
+            <li>
+               <img src="images/slides/portico-deco.jpg" alt="Portico Deco, Elba - Ornamento" />
+               <a href="" class="button">Ornamenti</a>
+            </li>
+            <li>
+               <img src="images/slides/samantha-and-spirit.jpg" alt="Samantha & Spirit- Dipinto su Commissione" />
+               <a href="" class="button">Dipinti</a>
+            </li>
+            <li>
+               <img src="images/slides/mobile-pappagalli.jpg" alt="Pappagalli - Decorazioni su Mobili" />
+               <a href="" class="button">Decorazioni su Mobili</a>
+            </li>
+            <li>
+               <img src="images/slides/corsi.jpg" alt="Corsi di Aerografo" />
+               <a href="" class="button">Corsi di Aerografo</a>
+            </li>
+         </ul>
+      </section>
+      <section id="intro" class="intro">
+         <div class="wrapper">
+            <h1>Un artista, due linguaggi</h1>
+            <div id="decorazioni" class="decorazioni">
+               <img src="images/testaGreca_small.jpg" width="60" />
+               <h2>Decorazioni e Dipinti</h2>
+               <p>Come pittore, mentre realizzo <a href="">affreschi</a>, <a href="">murales</a>, <a href="">trompe l’oeil</a> e <a href="">dipinti</a>, sento intensamente l'influenza della cultura italiana e la mia parte artistica e’ piu’ rivolta ad un atteggiamento classico.</p>
+               <p><a href="" class="button">Esplora</a></p>
+            </div>
+            <div id="kustomite" class="kustomite">
+               <img src="images/kustomite_small.jpg" width="60" />
+               <h2>Kustomite</h2>
+               <p>Come aerografista, l’influenza americana predomina. Mentre realizzo aerografie su <a href="">carrozzerie</a> e <a href="">caschi</a>, il pittore in me diventa un “trasformatore” che con la sua creativita’ tramuta quei singoli oggetti “comuni” in oggetti “originali ed unici: in una sola parola …kustom!</p>
+               <p><a href="" class="button">Esplora</a></p>
+            </div>
+         </div>
+      </section>   
+   </div>
+   <section id="tiles" class="tiles">
+      <ul>
+         <li>
+            <img src="images/slides/mauritius.jpg" alt="Isola Mauritius - Trompe L'Oeil dipinto a mano" />
+            <div class="caption">
+               <h2>Trompe L'Oeil</h2>
+            </div>
+         </li>
+         <li>
+            <img src="images/slides/sansone.jpg" alt="Spiaggia di Sansone, Elba - Murales dipinto a mano" />
+            <div class="caption">
+               <h2>Murales</h2>
+            </div>            
+         </li>
+         <li>
+            <img src="images/slides/bacco.jpg" alt="Bacco - Affresco" class="adjust" />
+            <div class="caption">
+               <h2>Affreschi</h2>
+            </div>
+         </li>
+         <li>
+            <img src="images/slides/portico-deco.jpg" alt="Decorazione porticato - Greche e Stemmi Araldici" class="adjust" />
+            <div class="caption">
+               <h2>Ornamenti</h2>
+            </div>
+         </li>
+         <li>
+            <img src="images/slides/samantha-and-spirit.jpg" alt="Samantha & Spirit - Ritratto su commissione" style="margin-left:-75%" />
+            <div class="caption">
+               <h2>Dipinti</h2>
+            </div>
+         </li>
+         <li>
+            <img src="images/slides/mobile-pappagalli.jpg" alt="Mobile Art Noveau - Decorazioni su mobili" />
+            <div class="caption">
+               <h2>Decorazione Mobili</h2>
+            </div>
+         </li>
+         <li>
+            <img src="images/Harley_Davidson_AMF80.jpg" alt="Harley Davidson AMF80 - Auto e Moto" />
+            <div class="caption">
+               <h2>Auto e Moto</h2>
+            </div>
+         </li>
+         <li>
+            <img src="images/casco_GT3_Cup.jpg" alt="Harley Davidson AMF80 - Auto e Moto" />
+            <div class="caption">
+               <h2>Caschi</h2>
+            </div>
+         </li>
+         <li>
+            <img src="images/giubbotto_Eagles.jpg" alt="Eagles - Giubbotti e Tshirts" class="reverse" />
+            <div class="caption">
+               <h2>Giubbotti e Tshirts</h2>
+            </div>
+         </li>
+         <li>
+           <img src="images/slides/corsi.jpg" alt="Corsi di Aerografo" class="adjust" />
+            <div class="caption">
+               <h2>Corsi di Aerografo</h2>
+            </div>
+         </li>
+      </ul>
+   </section>
+   <footer>
+      <div class="wrapper">
+         <p>
+            <a href="mailto:alex@alexlorenzi.com">alex@alexlorenzi.com</a> | 
+            <a href="#">348.6700.922</a>
+         </p>
+      </div>
+   </footer>
+</body>
+</html>
